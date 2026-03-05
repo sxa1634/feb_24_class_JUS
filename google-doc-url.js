8a8b07859383b57a8ed38abbbee0d77c6e15d897
sxa1634 <sxa1634@miami.edu>
2026-03-05 17:26:24 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/16x5z12f-7ZV2L8hz5sJn1pEQxSoR74KbMZJQhjhIHzk/edit?gid=0#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1YgYYzibt6-njPj7Asth3H-scwP8sfwUB6X0uC06l5BE/edit?gid=117736644#gid=117736644';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
